cdf06ed2381c1fb7adb6d5433bfc480dda30be3d
gpanaretou <gpanaretou@hotmail.com>
2023-08-12 11:34:15 +0300

    updated test to account for confirmation when deleting a quote

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -54,7 +54,12 @@ class QuotesTest < ApplicationSystemTestCase
     visit quotes_path
     assert_text @quote.name
 
-    click_on "Delete", match: :first
+    
+    
+    accept_confirm do
+      click_on "Delete", match: :first
+    end
+
     assert_no_text @quote.name
   end
   
