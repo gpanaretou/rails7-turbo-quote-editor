95a3a8b51b80f6083ee00cfd700c49179d9d3b0f
gpanaretou <gpanaretou@hotmail.com>
2023-08-17 16:07:46 +0300

    created initial stream from index quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,8 @@ class Quote < ApplicationRecord
     validates :name, presence: true
 
     scope :ordered, -> { order(id: :desc) }
+
+    # users subscribed to the quotes stream will have receive an update to the quotes (index of quotes) to show the new quotes
+    after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote",
+                            locals: { quote: self }, target: "quotes" }
 end
