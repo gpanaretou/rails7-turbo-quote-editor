d5e0db555623647ff2280c33e204821f1df5d175
gpanaretou <gpanaretou@hotmail.com>
2023-08-17 16:13:58 +0300

    code cleanup

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,6 +4,10 @@ class Quote < ApplicationRecord
     scope :ordered, -> { order(id: :desc) }
 
     # users subscribed to the quotes stream will have receive an update to the quotes (index of quotes) to show the new quotes
-    after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote",
-                            locals: { quote: self }, target: "quotes" }
+    
+    # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote",
+    #                         locals: { quote: self }, target: "quotes" }
+
+    # same as code above but more compact
+    after_create_commit -> { broadcast_prepend_to "quotes" }
 end
