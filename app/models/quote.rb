1c62230d66a531e52da8aa466b62a8a5dca9d7e7
gpanaretou <gpanaretou@hotmail.com>
2023-08-17 16:20:43 +0300

    added broadcasting after edit or destroy action of quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -10,4 +10,6 @@ class Quote < ApplicationRecord
 
     # same as code above but more compact
     after_create_commit -> { broadcast_prepend_to "quotes" }
+    after_update_commit -> { broadcast_replace_to "quotes" }
+    after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
