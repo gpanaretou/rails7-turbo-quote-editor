2cbb37a03d721dafad8d0809f0d6367e7ab4ca5d
gpanaretou <gpanaretou@hotmail.com>
2023-08-23 13:01:56 +0300

    added views and routes for line items in quotes

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -1,7 +1,7 @@
 class LineItemDate < ApplicationRecord
   belongs_to :quote
 
-  validates :date, presence: ture, uniqueness: { scope :quote_id }
+  validates :date, presence: true, uniqueness: { scope: :quote_id }
 
   scope :ordered, -> { order(date: :asc) }
 end
